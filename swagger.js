0b2db64d3c93b7c1d517b00834e42806406985fb
shweepps <shweepps@gmail.com>
2025-06-07 01:33:21 +0200

    gttp

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: 'API for managing book'
   },
   host: 'localhost:3000',
-  schemes: ['http','https']
+  schemes: ['https']
 };
 
 const outputFile = './swagger.json';
