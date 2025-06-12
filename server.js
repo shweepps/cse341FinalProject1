80ee0b928407a85544797c5c7a453602a11f2147
shweepps <shweepps@gmail.com>
2025-06-12 20:21:46 +0200

    mongo

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   store: MongoStore.create({
-    mongoUrl: process.env.MONGODB_URI,
+    mongoUrl: process.env.MONGODB_URL,
     collectionName: 'sessions'
   })
 }));
