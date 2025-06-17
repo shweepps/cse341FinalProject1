50fd21e37c3f2e9b127e0ec58f4cee303549f71e
shweepps <shweepps@gmail.com>
2025-06-17 22:42:46 +0200

    auth

--- routes/favorites.js
+++ routes/favorites.js
@@ -7,7 +7,6 @@ router.get('/', favoriteController.getAll);
 router.get('/:id', favoriteController.getSingle);
 
 router.post('/', ensreAuth, favoriteController.createFavorite);
-//router.put('/:id', ensreAuth, favoriteController.updateFavorite);
 router.delete('/:id', ensreAuth, favoriteController.deleteFavorite);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/users'); // adjust path if needed
+const userController = require('../controllers/users'); 
 const ensureAuth = require('../middleware/authMiddleware')
 
 
